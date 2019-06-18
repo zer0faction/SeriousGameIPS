ea05b8cdecd3be66630371b15220196ae1613aeb
Yoki <yboeckholtz@gmail.com>
2019-06-18 14:34:35 +0200

    Massive update.

---
+++ Assets/Scripts/Menus/CollectibleMenuScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectibleMenuScript : MonoBehaviour
+{
+    [SerializeField] private Button backButton;
+    [SerializeField] private Button goToRecipes;
+    [SerializeField] private Button goToTips;
+
+    [SerializeField] private GameObject UICanvasMainMenu;
+    [SerializeField] private GameObject UICanvasRecipes;
+    [SerializeField] private GameObject UICanvasTips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backButton.onClick.AddListener(BackToMainMenu);
+        goToRecipes.onClick.AddListener(GoToRecipes);
+        goToTips.onClick.AddListener(GoToLevelTips);
+    }
+
+    private void BackToMainMenu()
+    {
+        gameObject.SetActive(false);
+        UICanvasMainMenu.SetActive(true);
+    }
+
+    private void GoToLevelTips()
+    {
+        gameObject.SetActive(false);
+        UICanvasTips.SetActive(true);
+    }
+
+    private void GoToRecipes()
+    {
+        gameObject.SetActive(false);
+        UICanvasRecipes.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Menus/TipListController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TipListController : MonoBehaviour
+{
+    [SerializeField] private Text tipText;
+    [SerializeField] private Button buttonLeft;
+    [SerializeField] private Button buttonRight;
+    [SerializeField] private Button backButton;
+
+    [SerializeField] private GameObject UICanvasRecipeCollectibles;
+
+    private GameObject dontDestroy;
+    private List<string> tips;
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dontDestroy = GameObject.Find("DontDestroy");
+
+        index = 0;
+        tips = dontDestroy.GetComponent<DontDestroy>().GetTips();
+        buttonLeft.onClick.AddListener(goToTheLeft);
+        buttonRight.onClick.AddListener(goToTheRight);
+        backButton.onClick.AddListener(BackToRecipeMenu);
+
+        if(tips.Count == 0)
+        {
+            tipText.text = "Speel eerst de levels om tips te ontgrendelen!";
+        } else
+        {
+            changeTipText(tips[index]);
+        }
+    }
+
+    private void changeTipText(string text)
+    {
+        tipText.text = text;
+    }
+
+    private void goToTheLeft()
+    {
+        if (tips.Count == 0)
+        {
+            tipText.text = "Speel eerst de levels om tips te ontgrendelen!";
+        }
+        else
+        {
+            if (index == 0)
+            {
+                Debug.Log(index);
+                index = tips.Count - 1;
+                changeTipText(tips[index]);
+            }
+            else
+            {
+                Debug.Log(index);
+                index = index - 1;
+                changeTipText(tips[index]);
+            }
+        }
+    }
+
+    private void goToTheRight()
+    {
+        if (tips.Count == 0)
+        {
+            tipText.text = "Speel eerst de levels om tips te ontgrendelen!";
+        }
+        else
+        {
+            if (index == tips.Count - 1)
+            {
+                Debug.Log(index);
+                index = 0;
+                changeTipText(tips[index]);
+            }
+            else
+            {
+                Debug.Log(index);
+                index = index + 1;
+                changeTipText(tips[index]);
+            }
+        }
+    }
+
+    private void BackToRecipeMenu()
+    {
+        gameObject.SetActive(false);
+        UICanvasRecipeCollectibles.SetActive(true);
+    }
+}
